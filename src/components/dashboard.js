f8e1dc2605aa7a8727c70d978e1fa570cee1f0b4
Thomas Chun <tchun2055@gmail.com>
2019-05-14 17:06:52 -0700

    updated login auth and basic styling

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,31 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 import NavBar from './nav-bar';
+import dashboard from './dashboard';
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-
-    }
-
-    render() {
-        return (
-            <div className='main-container'>
-                <NavBar />
-                <div className="dashboard">
-                    <div className="dashboard-username">Username: {this.props.username}</div>
-                    <div className="dashboard-name">Name: {this.props.name}</div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='main-container'>
+        <NavBar />
+        <div className='dashboard'>
+          Dashboard
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    const { currentUser } = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Dashboard));
